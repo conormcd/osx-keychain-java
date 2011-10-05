74e792383da4e107ad5cdb5f312d7a0e6e59e343
Conor McDermottroe <conor@mcdermottroe.com>
2011-10-06 00:16:22 +0100

    Add addIntenetPassword(URL, String, String)

--- src/java/com/mcdermottroe/apple/OSXKeychain.java
+++ src/java/com/mcdermottroe/apple/OSXKeychain.java
@@ -32,6 +32,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /** An interface to the OS X Keychain. The names of functions and parameters
  *	will mostly match the functions listed in the <a href="http://developer.apple.com/library/mac/#documentation/Security/Reference/keychainservices/Reference/reference.html">Keychain Services Reference</a>.
@@ -82,6 +84,45 @@ public class OSXKeychain {
 	throws OSXKeychainException
 	{
 		_addGenericPassword(serviceName, accountName, password);
+	}
+
+	/** Add an internet password to the keychain.
+	 *
+	 *	@param	url						The URL to associate the password with.
+	 *	@param	accountName				The username for the password. Pass
+	 *									null here if the username is provided
+	 *									in the URL.
+	 *	@param	password				The password to add to the keychain.
+	 *	@throws	OSXKeychainException	If an error occurs when communicating
+	 *									with the OS X keychain.
+	 */
+	public void addInternetPassword(URL url, String accountName, String password)
+	throws OSXKeychainException
+	{
+		// Work out the username if we can.
+		String username = getUsername(accountName, url);
+		if (username == null) {
+			throw new OSXKeychainException("No account name supplied.");
+		}
+
+		// Figure out the protocol and port
+		int port = url.getPort();
+		OSXKeychainProtocolType protocol = getProtocol(port, url.getProtocol());
+		if (port <= 0) {
+			port = getPort(port, protocol);
+		}
+
+		// Now add the password
+		addInternetPassword(
+			url.getHost(),
+			null,
+			username,
+			url.getPath(),
+			port,
+			protocol,
+			OSXKeychainAuthenticationType.Any,
+			password
+		);
 	}
 
 	/** Add an intenet password to the keychain.
@@ -142,13 +183,7 @@ public class OSXKeychain {
 	public String findInternetPassword(URL url, String accountName)
 	throws OSXKeychainException
 	{
-		String username = accountName;
-		if (username == null) {
-			username = url.getUserInfo();
-			if (username.indexOf(':') > 0) {
-				username = username.substring(0, username.indexOf(':'));
-			}
-		}
+		String username = getUsername(accountName, url);
 		if (username == null) {
 			throw new OSXKeychainException("No account name supplied.");
 		}
@@ -225,8 +260,9 @@ public class OSXKeychain {
 	throws OSXKeychainException;
 
 	/** See Java_com_mcdermottroe_apple_OSXKeychain__1addInternetPassword for
-	 *	the implementation of this and use TODO
-	 *
+	 *	the implementation of this and use {@link #addInternetPassword(String,
+	 *	String, String, String, int, OSXKeychainProtocolType,
+	 *	OSXKeychainAuthenticationType, String)} to call this.
 	 */
 	private native void _addInternetPassword(String serverName, String securityDomain, String accountName, String path, int port, int protocol, int authenticationType, String password)
 	throws OSXKeychainException;
@@ -302,5 +338,136 @@ public class OSXKeychain {
 
 		// Now load the library
 		System.load(soInTmp.getAbsolutePath());
+	}
+
+	/* ********************************* */
+	/* Private utilities from here down. */
+	/* ********************************* */
+
+	/** A fixed mapping of ports to known protocols. */
+	private static final Map<Integer, OSXKeychainProtocolType> PROTOCOLS;
+	static {
+		PROTOCOLS = new HashMap<Integer, OSXKeychainProtocolType>(32);
+		PROTOCOLS.put(548, OSXKeychainProtocolType.AFP);
+		PROTOCOLS.put(3020, OSXKeychainProtocolType.CIFS);
+		PROTOCOLS.put(2401, OSXKeychainProtocolType.CVSpserver);
+		PROTOCOLS.put(3689, OSXKeychainProtocolType.DAAP);
+		PROTOCOLS.put(3031, OSXKeychainProtocolType.EPPC);
+		PROTOCOLS.put(21, OSXKeychainProtocolType.FTP);
+		PROTOCOLS.put(990, OSXKeychainProtocolType.FTPS);
+		PROTOCOLS.put(80, OSXKeychainProtocolType.HTTP);
+		PROTOCOLS.put(443, OSXKeychainProtocolType.HTTPS);
+		PROTOCOLS.put(143, OSXKeychainProtocolType.IMAP);
+		PROTOCOLS.put(993, OSXKeychainProtocolType.IMAPS);
+		PROTOCOLS.put(631, OSXKeychainProtocolType.IPP);
+		PROTOCOLS.put(6667, OSXKeychainProtocolType.IRC);
+		PROTOCOLS.put(994, OSXKeychainProtocolType.IRCS);
+		PROTOCOLS.put(389, OSXKeychainProtocolType.LDAP);
+		PROTOCOLS.put(636, OSXKeychainProtocolType.LDAPS);
+		PROTOCOLS.put(119, OSXKeychainProtocolType.NNTP);
+		PROTOCOLS.put(563, OSXKeychainProtocolType.NNTPS);
+		PROTOCOLS.put(110, OSXKeychainProtocolType.POP3);
+		PROTOCOLS.put(995, OSXKeychainProtocolType.POP3S);
+		PROTOCOLS.put(554, OSXKeychainProtocolType.RTSP);
+		PROTOCOLS.put(25, OSXKeychainProtocolType.SMTP);
+		PROTOCOLS.put(1080, OSXKeychainProtocolType.SOCKS);
+		PROTOCOLS.put(22, OSXKeychainProtocolType.SSH);
+		PROTOCOLS.put(3690, OSXKeychainProtocolType.SVN);
+		PROTOCOLS.put(23, OSXKeychainProtocolType.Telnet);
+		PROTOCOLS.put(992, OSXKeychainProtocolType.TelnetS);
+	}
+
+	/** Resolve a username from either a supplied username or from the username
+	 *	portion of a URL.
+	 *
+	 *	@param	username				The username to return if it's not
+	 *									null.
+	 *	@param	url						The URL to check iff username is null.
+	 *	@return							If the username is not null, the
+	 *									username will be returned. If the
+	 *									username is null and the url contains a
+	 *									username then the username from the URL
+	 *									will be returned.
+	 *	@throws	OSXKeychainException	If username is null and the URL
+	 *									contains no username.
+	 */
+	private static String getUsername(String username, URL url)
+	throws OSXKeychainException
+	{
+		if (username != null) {
+			return username;
+		}
+
+		String urlUsername = url.getUserInfo();
+		if (urlUsername != null) {
+			if (urlUsername.indexOf(':') > 0) {
+				return urlUsername.substring(0, urlUsername.indexOf(':'));
+			} else {
+				return urlUsername;
+			}
+		} else {
+			throw new OSXKeychainException("Could not return a username.");
+		}
+	}
+
+	/** Resolve a port from either a supplied port or from a protocol.
+	 *
+	 *	@param	port
+	 *	@param	protocol
+	 *	@return
+	 *	@throws	OSXKeychainException
+	 */
+	private static int getPort(int port, OSXKeychainProtocolType protocol)
+	throws OSXKeychainException
+	{
+		if (port > 0) {
+			return port;
+		}
+		for (Map.Entry<Integer, OSXKeychainProtocolType> entry : PROTOCOLS.entrySet()) {
+			if (entry.getValue() == protocol) {
+				return entry.getKey();
+			}
+		}
+		throw new OSXKeychainException("Could not determine port.");
+	}
+
+	/** Figure out the protocol given the port and or the protocol portion of
+	 *	the URL.
+	 *
+	 *	@param	port					An IP port number.
+	 *	@param	protocol				The protocol part of a URL.
+	 *	@return							The protocol type which matches either
+	 *									the port or the protocol prefix from
+	 *									the URL.
+	 *	@throws	OSXKeychainException	If there's no known protocol which
+	 *									matches the port or protocol prefix.
+	 */
+	private static OSXKeychainProtocolType getProtocol(int port, String protocol)
+	throws OSXKeychainException
+	{
+		// Try to figure it out from the port, if available.
+		for (Map.Entry<Integer, OSXKeychainProtocolType> pp : PROTOCOLS.entrySet()) {
+			if (pp.getKey() == port) {
+				return pp.getValue();
+			}
+		}
+
+		// See if we got a protocol prefix.
+		if (protocol != null) {
+			// Try to match the name of the protocol
+			for (OSXKeychainProtocolType pt : OSXKeychainProtocolType.values()) {
+				if (pt.toString().toLowerCase().equals(protocol.toLowerCase())) {
+					return pt;
+				}
+			}
+
+			// A few aliases/ones we couldn't handle in the map above.
+			if ("smb".equals(protocol)) {
+				return OSXKeychainProtocolType.SMB;
+			}
+		}
+
+		// Give up.
+		throw new OSXKeychainException("Could not determine protocol.");
 	}
 }
